127dc02a16a5d608a88a2d9c0de3e1a92b3821c6
Mauricio Sousa <morricedev@gmail.com>
2021-05-04 15:37:22 -0300

    Project base created

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: [
+    "../src/stories/*.stories.mdx",
+    "../src/stories/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/*.stories.mdx",
+    "../src/components/*.stories.@(js|jsx|ts|tsx)",
+    "../src/styles/*.stories.mdx",
+    "../src/styles/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-viewport",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs",
+  ],
+};
