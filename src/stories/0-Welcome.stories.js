f2a5559b2f94c68f66400e53ffbe447c10aac8d2
Mauricio Sousa <morricedev@gmail.com>
2021-05-04 15:43:20 -0300

    Add Welcome and Introduction stories

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default {
+  title: "Introduction / Welcome",
+};
+
+export const toAutoEscola = () => (
+  <div>
+    <h1>Auto Escola Nardini</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod officiis
+      quisquam, incidunt ex ipsam doloribus repellendus magnam laboriosam ad
+      excepturi perspiciatis ducimus totam dolore deleniti impedit debitis
+      fugiat eius quam!
+    </p>
+  </div>
+);
