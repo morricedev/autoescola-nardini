41577487959648dfb9c70c700085254027ead29c
Mauricio Sousa <morricedev@gmail.com>
2021-05-04 15:42:19 -0300

    Add Hero component

---
+++ src/components/Hero.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Hero from "./Hero";
+
+test("renders Hero children", () => {
+  const { /*debug,*/ getByText } = render(
+    <Hero>
+      <p>Morrice Sousa</p>
+    </Hero>
+  );
+
+  // debug();
+
+  expect(getByText("Morrice Sousa")).toBeInTheDocument(); /*toBeTruthy()*/
+});
+
+test("renders background image", () => {
+  const imageUrl = "https://picsum.photos/200";
+  const { getByTestId } = render(<Hero image={imageUrl} />);
+
+  expect(getByTestId("hero")).toHaveStyleRule({
+    backgroundImage: imageUrl,
+  });
+});
