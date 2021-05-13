581acac26a023b6a4146f5872cb3b4b4284303bb
Mauricio Sousa <morricedev@gmail.com>
2021-05-13 20:12:30 -0300

    Add Product Detail page

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -82,7 +82,7 @@ CardMediaDescription.propTypes = {
 const StyledCard = styled.div`
   background-color: #fff;
   border-radius: 4px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
+  border: 1px solid ${(props) => props.theme.colors.border};
   overflow: hidden;
 `;
 
